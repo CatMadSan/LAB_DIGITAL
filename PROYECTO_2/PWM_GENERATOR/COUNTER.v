97d9d2c53cc74e82f1eb88c4e9471815fa4527ab
CatMadSan <99616267+CatMadSan@users.noreply.github.com>
2022-05-07 19:10:01 -0600

    Update COUNTER.v

--- PROYECTO_2/PWM_GENERATOR/COUNTER.v
+++ PROYECTO_2/PWM_GENERATOR/COUNTER.v
@@ -22,13 +22,18 @@
 
 module COUNTER(
     input CLoK,
+    input Reset,
     output [3:0] CNTR
     );
     
     reg [3:0] PRV = 0;
     
-    always @ (posedge CLoK)
-    PRV <= PRV + 1; // De ser un número mayor a 15, simplemente ocurrirá overflow y quedará en 4'b0000
+    always @ (posedge CLoK) begin
+    case (Reset)
+        0 : PRV <= PRV + 1; // De ser un número mayor a 15, simplemente ocurrirá overflow y quedará en 4'b0000
+        1 : PRV <= 4'b0000;
+    endcase
+    end
     
     assign CNTR = PRV;
     
